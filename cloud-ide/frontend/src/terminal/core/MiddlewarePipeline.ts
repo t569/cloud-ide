c57dbde347686d67ae03759d5c5f863d8ed778e5
t569 <timothyobrike@gmail.com>
2026-03-16 12:36:38 +0100

    added middleware class for sanitisingtransported input/output

---
+++ cloud-ide/frontend/src/terminal/core/MiddlewarePipeline.ts
@@ -0,0 +1,32 @@
+export interface IMiddleware {
+    name: string;
+
+    // Manipulates/processes data from backend/WASM before it hits the Ui
+    processIncoming(data: string): string;
+
+    // Manipulates/processes data coming from the UI before it hits the backend
+    processOutgoing(data: string):string;
+
+}
+
+export class MiddlewarePipeline {
+    // list of all or available middlewares
+    private middlewares: IMiddleware[] = [];
+
+
+    // Registers a new middleware plugin into the pipleine
+    public use(middleware: IMiddleware):void{
+        this.middlewares.push(middleware);
+    }
+
+    // Runs our incoming data through all registered middlewares sequetially
+    public processIncoming(data:string):string {
+        return this.middlewares.reduce((currentData, mw) => mw.processIncoming(currentData), data);
+    }
+
+    // Runs our outgoing data through all regsitered middlewares sequentially
+    public processOutgoing(data: string): string {
+        return this.middlewares.reduce((currentData, mw) => mw.processOutgoing(currentData), data);
+    }
+
+}

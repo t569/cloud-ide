55f6978aafc1fbea80885c2b667e3358bc6e98f5
t569 <timothyobrike@gmail.com>
2026-03-17 12:46:14 +0100

    feat: added interface for database helpers

---
+++ cloud-ide/backend/src/database/ISessionRepository.ts
@@ -0,0 +1,9 @@
+import { SessionRecord } from './models';
+
+export interface ISessionRepository {
+  save(session: SessionRecord): Promise<void>;
+  get(sessionId: string): Promise<SessionRecord | null>;
+  getSessionsByEnvId(envId: string): Promise<SessionRecord[]>; // The crucial relational link!
+  updateStatus(sessionId: string, status: SessionRecord['status']): Promise<void>;
+  delete(sessionId: string): Promise<void>;
+}

0f0d3fcfaa29a9cc6ba4f741871d8a88c995bfd4
t569 <timothyobrike@gmail.com>
2026-03-17 15:51:57 +0100

    refactor: updated the main server.ts

--- cloud-ide/backend/src/server.ts
+++ cloud-ide/backend/src/server.ts
@@ -10,6 +10,13 @@ import { SessionManager } from './core/SessionManager';
 // Import our Transports (REST & WebSockets)
 import { WebSocketManager } from './transport/WebSocketManager';
 import { createSessionRouter } from './api/SessionRoutes'; 
+import { createEnvironmentRouter } from './api/EnvironmentRoutes';
+
+// Import our database functionality
+import { JsonEnvironmentRepository } from './database/json/JsonEnvironmentRepository';
+import { JsonSessionRepository } from './database/json/JsonSessionRepository';
+import { PersistenceLayer } from './database/PersistenceLayer';
+
 
 const app = express();
 
@@ -17,33 +24,32 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Allows Express to parse JSON bodies in POST requests
 
-// ==========================================
-// THE KERNEL (Dependency Injection)
-// ==========================================
+// 1. Initialize Storage (ROM)
+// TODO: pass in a string for the path of our database JSON files
+const envRepo = new JsonEnvironmentRepository();
+const sessionRepo = new JsonSessionRepository();
 
-// 1. Instantiate the Global Process Registry exactly once
+// 2. Initialize Core Kernel (RAM)
 const sessionManager = new SessionManager();
 
-// 2. The Control Plane (HTTP REST API)
+// 3. Initialize Background Daemon
+const persistenceLayer = new PersistenceLayer(sessionManager, sessionRepo);
+
+// 4. Mount Control Plane (HTTP API)
 // We pass the sessionManager into the router so it can Stop/Destroy containers
-app.use('/api/session', createSessionRouter(sessionManager));
+app.use('/api/session', createSessionRouter(sessionManager)); // the session manager is also passed here and we can do stuff with it
+app.use('/api/environment', createEnvironmentRouter(envRepo, sessionRepo)); // this helps with environments
 
-// 3. Create the underlying Node HTTP Server
+// 5. Mount Data Plane (WebSockets)
 const server = http.createServer(app);
-
-// 4. The Data Plane (WebSockets)
-// We pass the EXACT SAME sessionManager to the WebSocket layer
 const wss = new WebSocketServer({ server });
-const wsManager = new WebSocketManager(sessionManager);
+const wsManager = new WebSocketManager(sessionManager, envRepo, sessionRepo);
 
 wss.on('connection', (ws, req) => {
-  wsManager.handleConnection(ws, req);
+  wsManager.handleConnection(ws, req);  // initialises the sessions and container stuff: IT ALL STARTS HERE!
 });
 
-// ==========================================
-// IGNITION
-// ==========================================
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-  console.log(`\x1b[1;32m[Kernel]\x1b[0m Cloud IDE Backend initialized on port ${PORT}`);
+  console.log(`\x1b[1;32m[Bootloader]\x1b[0m IDE OS initialized on port ${PORT}`);
 });

54a34edeadd168d61b5ad3566a94fc20a23071f1
t569 <timothyobrike@gmail.com>
2026-03-17 10:29:14 +0100

    fix: fixed the import bug in websocket manager

--- cloud-ide/backend/src/transport/WebSocketManager.ts
+++ cloud-ide/backend/src/transport/WebSocketManager.ts
@@ -1,42 +1,19 @@
-/* src/transport/WebSocketManager.ts
-*/
-
+// backend/src/transport/WebSocketManager.ts
 import { WebSocket, RawData } from 'ws';
 import { IncomingMessage } from 'http';
+
+// Import our IaC Pipeline
 import { DockerBuilder } from '../services/DockerBuilder';
-// import { ConfigParser, DockerGenerator } from '@cloud-ide/shared'; // FIX this
+import { ConfigParser, DockerGenerator } from '@cloud-ide/shared'; 
 
-
-// Import our new Core Architecture
+// Import our Core OS Architecture
 import { SessionManager } from '../core/SessionManager';
 import { Container } from '../core/Container';
 
+
+// this manages connection to our session objects
 export class WebSocketManager{
-  // We inject the global SessionManager (The Hashmap) into the websocket router
   constructor(private sessionManager: SessionManager) {}
 
-  public async handleConnection(ws: WebSocket, req: IncomingMessage): Promise<void> {
-    const url = new URL(req.url || '', `http://localhost`);
-
-    // The frontend MUST pass a sessionId now so we can track the coupling
-    const sessionId = url.searchParams.get('sessionId');
-    const envName = url.searchParams.get('env');
-
-    if (!sessionId) {
-      ws.send('\r\n\x1b[1;31mError: Missing sessionId in connection request.\x1b[0m\r\n');
-      ws.close();
-      return;
-    }
-
-    try{
-      // Workflow goes here
-    }catch (err: any) {
-      ws.send(`\r\n\x1b[1;31mInfrastructure Error: ${err.message}\x1b[0m\r\n`);
-    }
-  }
-
-  private async fetchConfigFromDB(envName: string): Promise<string> {
-    // return a default environment config
-    return JSON.stringify({ baseImage: 'node:18-bullseye', system: [], languages: {} });
-  }
+  public async handleConnection(ws: WebSocket, req: IncomingMessage): Promise<void> {}
 }

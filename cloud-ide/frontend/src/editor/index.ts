06f97e056869d4e2755f48f31a4638d75538ee3b
t569 <timothyobrike@gmail.com>
2026-03-18 20:46:28 +0100

    feat: export our components for use

---
+++ cloud-ide/frontend/src/editor/index.ts
@@ -0,0 +1,4 @@
+// frontend/src/editor/index.ts
+
+// This is where we will export editor component for use
+export { default as WorkspaceEditor } from './components/WorkspaceEditor';
--- cloud-ide/frontend/src/terminal/index.ts
+++ cloud-ide/frontend/src/terminal/index.ts
@@ -1,1 +1,5 @@
+// frontend/src/terminal/index.ts 
+
 // This is where we will export the terminal component for use
+
+export {TerminalComponent } from './components/Terminal';

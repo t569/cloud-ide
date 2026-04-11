d046031a3a4ca00e4663766d0e225d19db5123ec
tim569 <timothyobrike@gmail.com>
2026-04-11 17:29:04 +0100

    docs: depreciated

--- cloud-ide/src/backend/src/services/sandbox/ISandboxDriver.ts
+++ cloud-ide/src/backend/src/services/sandbox/ISandboxDriver.ts
@@ -1,5 +1,6 @@
 import { InternalBootRequest, InternalBootResponse } from '../OpenSandboxRouter'; // Reusing your types!
 
+// DEPRECIATED: we are using rust for this now
 export interface ISandboxDriver {
   bootContainer(request: InternalBootRequest): Promise<InternalBootResponse>;
   pauseSandbox(sandboxId: string): Promise<boolean>;

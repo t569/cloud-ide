48855f3642862f4a0569b9e1088e29e654be1091
tim569 <timothyobrike@gmail.com>
2026-04-11 17:39:45 +0100

    feat: re-export database types

---
+++ cloud-ide/src/backend/src/database/interfaces/index.ts
@@ -0,0 +1,9 @@
+import { IEnvironmentRepository } from "./IEnvironmentRepository";
+import { ISessionRepository } from "./ISessionRepository";
+import { ISandboxRepository } from "./ISandboxRepository";
+
+
+
+export type { IEnvironmentRepository};
+export type {ISandboxRepository};
+export type {ISessionRepository};
